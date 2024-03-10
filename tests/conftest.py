9303b0e8a7f19257dada437044acbef9cb54fb6f
ITS3DB-PTL-CLN0\lungo <lung.ngo@cpmojo.com>
2024-03-11 00:28:37 +0700

    Update action workflow

--- tests/conftest.py
+++ tests/conftest.py
@@ -62,9 +62,13 @@ def pytest_runtest_makereport(item, call):
     # execute all other hooks to obtain the report object
     outcome = yield
     rep = outcome.get_result()
+    if not os.path.exists("test_artifacts/report"):
+        os.makedirs("test_artifacts/report")
 
     if rep.when == "call" and rep.failed:
         # A test failed, let's capture a screenshot
+        if not os.path.exists("test_artifacts/screenshots"):
+            os.makedirs("test_artifacts/screenshots")
         browser = item.funcargs['browser']
         contexts = browser.contexts
         for context in contexts:
--- tests/utils/logger.py
+++ tests/utils/logger.py
@@ -1,4 +1,5 @@
 import logging
+import os
 from tests.utils.date_helper import get_current_time_str
 
 
@@ -7,6 +8,8 @@ class TestLogger:
         self.logger = logging.getLogger(test_name)
         self.logger.setLevel(logging.DEBUG)
         formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        if not os.path.exists("test_artifacts/test_logs"):
+            os.makedirs("test_artifacts/test_logs")
         file_handler = logging.FileHandler(f'test_artifacts/test_logs/{test_name}_{get_current_time_str()}.log')
         file_handler.setLevel(logging.DEBUG)
         file_handler.setFormatter(formatter)
